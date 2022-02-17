4145f2aded372bc3197a164ec428c8b385a5a0b9
KadirSheikh <rs7246510@gmail.com>
2022-02-17 11:44:34 +0530

    First Project-Budget Calculator

---
+++ budget-calculator/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Cards from "../UI/Cards";
+
+function ExpenseItem(props) {
+  return (
+    <li>
+      <Cards className="expense-item">
+        <ExpenseDate date={props.date} />
+
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">$ {props.amount}</div>
+        </div>
+      </Cards>
+    </li>
+  );
+}
+
+export default ExpenseItem;
