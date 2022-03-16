3b252591634963775b3dd93452ffd4950fb23b4e
KadirSheikh <rs7246510@gmail.com>
2022-03-16 13:18:00 +0530

    Added Firebase as dummy db,added fetch() POST request to firebase

--- http_request_example/src/App.js
+++ http_request_example/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -27,23 +28,37 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-example-c2a62-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
 
       const data = await response.json();
-      const transformData = data.results.map((result) => {
-        return {
-          id: result.episode_id,
-          title: result.title,
-          openingText: result.opening_crawl,
-          releaseDate: result.release_date,
-        };
-      });
 
-      setMovies(transformData);
+      const loadedData = [];
+
+      for (const key in data) {
+        loadedData.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformData = data.results.map((result) => {
+      //   return {
+      //     id: result.episode_id,
+      //     title: result.title,
+      //     openingText: result.opening_crawl,
+      //     releaseDate: result.release_date,
+      //   };
+      // });
+
+      setMovies(loadedData);
     } catch (error) {
       setError(error.message);
     }
@@ -53,6 +68,22 @@ function App() {
   useEffect(() => {
     fetchMovieHandler();
   }, [fetchMovieHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-example-c2a62-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>No movies found.</p>;
 
@@ -70,6 +101,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
