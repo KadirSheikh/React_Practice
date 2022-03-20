c81bb8d81bf53e0453ae5ce3c6d42388a772af97
KadirSheikh <rs7246510@gmail.com>
2022-03-20 17:16:15 +0530

    Form validation using custom hooks

--- http-request-using-custom-hooks/src/App.js
+++ http-request-using-custom-hooks/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 import useHttp from "./hooks/use-http";
@@ -9,33 +8,7 @@ function App() {
 
   const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-  // const fetchTasks = async (taskText) => {
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       "https://react-http-example-c2a62-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error("Request failed!");
-  //     }
-
-  //     const data = await response.json();
-
-  //     const loadedTasks = [];
-
-  //     for (const taskKey in data) {
-  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-  //     }
-
-  //     setTasks(loadedTasks);
-  //   } catch (err) {
-  //     setError(err.message || "Something went wrong!");
-  //   }
-  //   setIsLoading(false);
-  // };
-
+  
   useEffect(() => {
     const transformData = (taskObj) => {
       const loadedTasks = [];
